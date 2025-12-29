25ae3018a26dbe1a7503bb9c0bde8247828ebfd7
djaaron2so <djaaron2so@gmail.com>
2025-12-29 02:34:22 -0800

    chore: remove temp r2 env debug logs

--- src/index.js
+++ src/index.js
@@ -32,13 +32,6 @@ const __dirname = path.dirname(__filename);
 // App
 // ================================
 const app = express();
-// ===== DEBUG R2 ENV (TEMPORAL) =====
-console.log("R2_ACCOUNT_ID?", !!process.env.R2_ACCOUNT_ID);
-console.log("R2_ACCESS_KEY_ID?", !!process.env.R2_ACCESS_KEY_ID);
-console.log("R2_SECRET_ACCESS_KEY?", !!process.env.R2_SECRET_ACCESS_KEY);
-console.log("R2_BUCKET?", !!process.env.R2_BUCKET);
-console.log("R2_PUBLIC_BASE?", !!process.env.R2_PUBLIC_BASE);
-// ==================================
 
 // ================================
 // Version / deploy id
